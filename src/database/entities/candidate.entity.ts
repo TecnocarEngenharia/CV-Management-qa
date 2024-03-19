99cced7bab322d218d6947699a4db84815dde019
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-03-19 14:26:49 -0300

    feat: nullable edit true

--- src/database/entities/candidate.entity.ts
+++ src/database/entities/candidate.entity.ts
@@ -112,34 +112,34 @@ export class Candidate {
   @Column({ nullable: false, type: 'varchar' })
   tipo_cnpj: string;
 
-  @Column({ nullable: false, type: 'varchar' })
+  @Column({ nullable: true, type: 'varchar' })
   vaga_100_presencial_porto_real_rj: string;
 
   @Column({ type: 'varchar', nullable: true })
   vaga_hibrida_porto_real_rj: string;
 
-  @Column({ nullable: false, type: 'varchar' })
+  @Column({ nullable: true, type: 'varchar' })
   vaga_100_presencial_goiana_pe: string;
 
   @Column({ type: 'varchar', nullable: true })
   vaga_hibrida_goiana_pe: string;
 
-  @Column({ nullable: false, type: 'varchar' })
+  @Column({ nullable: true, type: 'varchar' })
   vaga_100_presencial_betim_mg: string;
 
-  @Column({ nullable: false, type: 'varchar' })
+  @Column({ nullable: true, type: 'varchar' })
   vaga_hibrida_betim: string;
 
-  @Column({ nullable: false, type: 'varchar' })
+  @Column({ nullable: true, type: 'varchar' })
   vaga_100_presencial_sao_paulo: string;
 
   @Column({ type: 'varchar', nullable: true })
   vaga_hibrida_sao_paulo: string;
 
-  @Column({ nullable: false, type: 'varchar' })
+  @Column({ nullable: true, type: 'varchar' })
   vaga_internacional: string;
 
-  @Column({ nullable: false, type: 'varchar' })
+  @Column({ nullable: true, type: 'varchar' })
   home_office: string;
 
   @Column({ nullable: true, type: 'varchar' })
